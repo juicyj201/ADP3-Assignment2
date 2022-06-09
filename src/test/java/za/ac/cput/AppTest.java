4a017df579129926aa95516de8d245643e288e28
juicyj201 <220102473@mycput.ac.za>
2022-06-09 02:44:01 +0200

    Initial commit

---
+++ src/test/java/za/ac/cput/AppTest.java
@@ -0,0 +1,18 @@
+package za.ac.cput;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assertions.assertTrue( true );
+    }
+}
