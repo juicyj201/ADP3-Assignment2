3880377267d10b33241f57c73483d0db237ce4aa
Matthew Jones <220077681@mycput.ac.za>
2022-06-10 19:54:48 +0200

    Question 2a complete

---
+++ src/main/java/za/ac/cput/Factory/NameFactory.java
@@ -0,0 +1,28 @@
+package za.ac.cput.Factory;
+
+import za.ac.cput.Domain.Entities.Name;
+import za.ac.cput.util.helper;
+
+/**
+ * Matthew Jones
+ * 220077681
+ * Question 2
+ */
+
+public class NameFactory
+{
+    public static Name createName(String firstName, String middleName, String lastName) {
+
+        if (helper.isNullorEmpty(firstName))
+            return null;
+
+        if (helper.isNullorEmpty(lastName))
+            return null;
+
+        return new Name.NameBuilder()
+                .setFirstName(firstName)
+                .setMiddleName(middleName)
+                .setLastName(lastName)
+                .build();
+    }
+}
