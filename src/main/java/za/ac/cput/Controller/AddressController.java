8deac93434de7f2e665026f16ee1acb5ef75c969
juicyj201 <220102473@mycput.ac.za>
2022-06-16 20:58:58 +0200

    Adding changes

--- src/main/java/za/ac/cput/Controller/AddressController.java
+++ src/main/java/za/ac/cput/Controller/AddressController.java
@@ -1,17 +1,21 @@
 package za.ac.cput.Controller;
 
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import za.ac.cput.Domain.Entities.Address;
-import za.ac.cput.Repository.Interface.AddressRepository;
 import za.ac.cput.Service.Impl.AddressServiceImpl;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("school-management/address/")
 public class AddressController {
-    private AddressServiceImpl service;
+    private final AddressServiceImpl service;
 
     @Autowired
     public AddressController(AddressServiceImpl service){
@@ -19,22 +23,31 @@ public class AddressController {
     }
 
     @PostMapping("save")
-    private Address save(Address address){
-        return this.service.save(address);
+    private ResponseEntity<Address> save(Address address){
+        Address save = service.save(address);
+        return ResponseEntity.ok(save);
     }
 
     @GetMapping("read/{address}")
-    private Optional<Address> read(@RequestBody Address address){
-        return this.service.read(address);
+    private ResponseEntity<Address> read(@RequestBody Address address){
+        Address addr = this.service.read(address)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return ResponseEntity.ok(addr);
     }
 
     @GetMapping("readAll")
-    private List<Address> readAll(){
-        return this.service.readAll();
+    private ResponseEntity<List<Address>> readAll(){
+        List<Address> addrList = this.service.readAll();
+        return ResponseEntity.ok(addrList);
     }
 
     @DeleteMapping("delete")
-    private void delete(@RequestBody Address address){
-        this.service.delete(address);
+    private ResponseEntity<Void> delete(@RequestBody Address address){
+        Optional<Address> addr = this.service.read(address);
+        if(addr.isPresent() == true) {
+            this.service.delete(addr.get());
+        }
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/za/ac/cput/Controller/EmployeeController.java
+++ src/main/java/za/ac/cput/Controller/EmployeeController.java
@@ -9,9 +9,10 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("school-management/employee")
 public class EmployeeController {
     /**
-    private EmployeeServiceImpl service;
+    private final EmployeeServiceImpl service;
 
     @Autowired
     public EmployeeController(EmployeeServiceImpl service){
@@ -19,41 +20,52 @@ public class EmployeeController {
     }
 
     @PostMapping("save")
-    private Employee save(Employee employee){
-        return this.service.save(employee);
+    private ResponseEntity<Employee> save(Employee employee){
+    Employee save = service.save(employee);
+    return ResponseEntity.ok(save);
     }
 
     @GetMapping("read/{employee}")
-    private Optional<Employee> read(@PathVariable Employee employee){
-        return this.service.read(employee);
+    private ResponseEntity<Employee> read(@PathVariable Employee employee){
+    Employee emp = this.service.read(address)
+    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    return ResponseEntity.ok(emp);
     }
 
     @GetMapping("readAll")
-    private List<Employee> readAll(){
-        return this.service.readAll();
+    private ResponseEntity<List<Employee>> readAll(){
+    List<Employee> empList = this.service.readAll();
+    return ResponseEntity.ok(empList);
     }
 
     @DeleteMapping("delete")
-    private void delete(@RequestBody Employee employee){
-        this.service.delete(employee);
+    private ResponseEntity<Void> delete(@RequestBody Employee employee){
+    Optional<Employee> emp = this.service.read(employee);
+    if(emp.isPresent() == true) {
+    this.service.delete(emp.get());
+    }
+
+    return ResponseEntity.noContent().build();
     }
 
     @GetMapping("getEmployeeNameByEmail/{employeeEmail}")
-    public List<String> getEmployeeNameByEmail(@PathVariable String employeeEmail){
+    public ResponseEntity<List<String>> getEmployeeNameByEmail(@PathVariable String employeeEmail){
         //This is a method that is static and can be called from within the EmployeeServiceImpl class.
         //The method returns a list of employee names, instead of employee objects. It is almost
         //exactly the same as the read but is static.
 
-        return EmployeeServiceImpl.getEmployeeNameByEmail(employeeEmail);
+        List<String> empNames = EmployeeServiceImpl.getEmployeeNameByEmail(employeeEmail);
+        return ResponseEntity.ok(empName);
     }
 
      @GetMapping("getEmployeeNameByCity/{cityId}")
-     public List<String> getEmployeeNameByCity(@PathVariable String cityId){
+     public ResponseEntity<List<String>> getEmployeeNameByCity(@PathVariable String cityId){
         //This is a method that is static and can be called from within the EmployeeServiceImpl class.
         //The method returns a list of employee names, instead of employee objects. It is almost
         //exactly the same as the read but is static.
 
-        return EmployeeServiceImpl.getEmployeeNameByCity(cityId);
+        List<String> empNames = EmployeeServiceImpl.getEmployeeNameByCity(cityId);
+        return ResponseEntity.ok(empNames);
      }
 
     **/
